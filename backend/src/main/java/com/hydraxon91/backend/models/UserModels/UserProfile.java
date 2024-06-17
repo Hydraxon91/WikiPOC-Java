99be6d3722845ac9ceaa4d31380214bd3d737671
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-17 17:31:03 +0200

    Creates UserProfile Model

---
+++ backend/src/main/java/com/hydraxon91/backend/models/UserModels/UserProfile.java
@@ -0,0 +1,75 @@
+﻿package com.hydraxon91.backend.models.UserModels;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @JsonIgnore
+    @Column(name = "user_id")
+    private String userId;
+
+    private String userName;
+    private String profilePicture;
+    private String displayName;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "userProfile")
+    private ApplicationUser user;
+
+    // Getters and Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public ApplicationUser getUser() {
+        return user;
+    }
+
+    public void setUser(ApplicationUser user) {
+        this.user = user;
+    }
+}
