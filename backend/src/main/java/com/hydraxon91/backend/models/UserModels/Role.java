0012f0eb13b8cb35820d5a2686aee43ee6075070
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-17 17:30:43 +0200

    Creates Role Model

---
+++ backend/src/main/java/com/hydraxon91/backend/models/UserModels/Role.java
@@ -0,0 +1,51 @@
+﻿package com.hydraxon91.backend.models.UserModels;
+
+import org.springframework.security.core.GrantedAuthority;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<ApplicationUser> users = new HashSet<>();
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    // Getters and Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<ApplicationUser> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<ApplicationUser> users) {
+        this.users = users;
+    }
+}
