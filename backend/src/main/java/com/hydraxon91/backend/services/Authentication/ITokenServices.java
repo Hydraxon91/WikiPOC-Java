c8793f1e84d8ad7e18a4d6f65e49323c024275e9
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-17 17:46:10 +0200

    Implements ITokenServices interface

--- backend/src/main/java/com/hydraxon91/backend/services/Authentication/ITokenServices.java
+++ backend/src/main/java/com/hydraxon91/backend/services/Authentication/ITokenServices.java
@@ -1,4 +1,8 @@
 ﻿package com.hydraxon91.backend.services.Authentication;
 
+import com.hydraxon91.backend.models.UserModels.ApplicationUser;
+import com.hydraxon91.backend.models.UserModels.Role;
+
 public interface ITokenServices {
+    String createToken(ApplicationUser user, Role role);
 }
---
+++ backend/src/main/java/com/hydraxon91/backend/services/Authentication/TokenService.java
@@ -0,0 +1,44 @@
+﻿package com.hydraxon91.backend.services.Authentication;
+
+import com.hydraxon91.backend.models.UserModels.ApplicationUser;
+import com.hydraxon91.backend.models.UserModels.Role;
+import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class TokenService implements  ITokenServices{
+    @Value("${jwt.valid-issuer}")
+    private String jwtIssuer;
+    
+    @Value("${jwt.valid-audience}")
+    private String jwtAudience;
+    
+    @Value("${jwt.token-time}")
+    private int jwtExpiration;
+    
+    @Override
+    public String createToken(ApplicationUser user, Role role){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + jwtExpiration * 1000);
+        
+        Key key = Keys.hmacShaKeyFor(jwtIssuer.getBytes());
+        
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .claim("username", user.getUsername())
+                .claim("email", user.getEmail())
+                .claim("role", role.getName())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .setIssuer(jwtIssuer)
+                .setAudience(jwtAudience)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
