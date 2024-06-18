0e277c71ab6a1f553d08f18eb74b4e3e2b4861ad
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-18 10:38:27 +0200

    adds @ComponentScan(basePackages = {"com.hydraxon91.backend.services"}) to Backendapplication.java

--- backend/src/main/java/com/hydraxon91/backend/BackendApplication.java
+++ backend/src/main/java/com/hydraxon91/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.hydraxon91.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.hydraxon91.backend.services"})
 public class BackendApplication {
 
 	public static void main(String[] args) {
