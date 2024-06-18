e275707495f77702cc70821eec8c10ca5d48593e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-18 09:27:26 +0200

    Creates IAutchService interface

---
+++ backend/src/main/java/com/hydraxon91/backend/services/Authentication/IAutchService.java
@@ -0,0 +1,8 @@
+﻿package com.hydraxon91.backend.services.Authentication;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface IAutchService {
+    CompletableFuture<AuthResult> registerAsync(String email, String username, String password);
+    CompletableFuture<AuthResult> loginAsync(String username, String password);
+}
