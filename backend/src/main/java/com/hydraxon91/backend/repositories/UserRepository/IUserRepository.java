f88198e71fe5ec0b933925b06b28b7af26b29b4e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-18 09:40:35 +0200

    Creates IUserRepository.java interface

---
+++ backend/src/main/java/com/hydraxon91/backend/repositories/UserRepository/IUserRepository.java
@@ -0,0 +1,18 @@
+﻿package com.hydraxon91.backend.repositories.UserRepository;
+
+import com.hydraxon91.backend.models.UserModels.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<ApplicationUser, UUID>{
+    Optional<ApplicationUser> findByEmail(String email);
+    Optional<ApplicationUser> findByUsername(String username);
+
+    @Modifying
+    @Query("UPDATE ApplicationUser u SET u.username = ?1 WHERE u.id = ?2")
+    int updateUsernameById(String username, UUID id);
+}
