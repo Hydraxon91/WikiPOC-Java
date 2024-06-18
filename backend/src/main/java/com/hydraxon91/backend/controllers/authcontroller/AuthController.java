ba417bcf6df07ca5c47b17d507ac9894a7af4e39
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-18 19:46:00 +0200

    Update AuthController.java

--- backend/src/main/java/com/hydraxon91/backend/controllers/authcontroller/AuthController.java
+++ backend/src/main/java/com/hydraxon91/backend/controllers/authcontroller/AuthController.java
@@ -5,51 +5,43 @@ import com.hydraxon91.backend.services.Authentication.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/public/auth")
 public class AuthController {
     private final IAuthService authService;
-    
+
     @Autowired
     public AuthController(IAuthService authService) {
         this.authService = authService;
     }
-    
+
     @PostMapping("/register")
-    public ResponseEntity<AuthResult> register(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
-        try {
-            CompletableFuture<AuthResult> authResultCompletableFuture = authService.registerAsync(email, username, password);
-            AuthResult authResult = authResultCompletableFuture.get();
-            if (authResult.isSuccess()) {
-                return ResponseEntity.ok(authResult);
-            }
-            else return ResponseEntity.badRequest().body(authResult);
-        } catch (InterruptedException | ExecutionException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    public CompletableFuture<ResponseEntity<AuthResult>> register(@RequestParam String email, @RequestParam String username, @RequestParam String password) {
+        return authService.registerAsync(email, username, password)
+                .thenApply(authResult -> {
+                    if (authResult.isSuccess()) {
+                        return ResponseEntity.ok(authResult);
+                    } else {
+                        return ResponseEntity.badRequest().body(authResult);
+                    }
+                })
+                .exceptionally(ex -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
-    
+
     @PostMapping("/login")
-    public ResponseEntity<AuthResult> login(@RequestParam String usernameOrEmail, @RequestParam String password) {
-        try {
-            CompletableFuture<AuthResult> authResultFuture = authService.loginAsync(usernameOrEmail, password);
-            AuthResult authResult = authResultFuture.get(); // Block and wait for CompletableFuture to complete
-            if (authResult.isSuccess()) {
-                return ResponseEntity.ok(authResult);
-            } else {
-                return ResponseEntity.badRequest().body(authResult);
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            // Handle exceptions
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    public CompletableFuture<ResponseEntity<AuthResult>> login(@RequestParam String usernameOrEmail, @RequestParam String password) {
+        return authService.loginAsync(usernameOrEmail, password)
+                .thenApply(authResult -> {
+                    if (authResult.isSuccess()) {
+                        return ResponseEntity.ok(authResult);
+                    } else {
+                        return ResponseEntity.badRequest().body(authResult);
+                    }
+                })
+                .exceptionally(ex -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 }
