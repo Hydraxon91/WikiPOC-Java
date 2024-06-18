7d7045745baf557b93c5ffccf09174605c38d06e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-18 09:24:13 +0200

    Creates AuthResult.java

---
+++ backend/src/main/java/com/hydraxon91/backend/services/Authentication/AuthResult.java
@@ -0,0 +1,45 @@
+﻿package com.hydraxon91.backend.services.Authentication;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthResult {
+    private final boolean success;
+    private final String email;
+    private final String username;
+    private final String token;
+    private final Map<String, String> errorMessages;
+    
+    public AuthResult(boolean success, String email, String username, String token) {
+        this.success = success;
+        this.email = email;
+        this.username = username;
+        this.token = token;
+        this.errorMessages = new HashMap<>();
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Map<String, String> getErrorMessages() {
+        return Collections.unmodifiableMap(errorMessages);
+    }
+
+    public void addErrorMessage(String key, String message) {
+        errorMessages.put(key, message);
+    }
+}
