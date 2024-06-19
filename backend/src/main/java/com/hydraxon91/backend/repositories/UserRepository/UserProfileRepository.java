ce05682f39f0b579c80798e26ff876001974ca9a
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-19 18:24:34 +0200

    Updates UserProfileRepository

--- backend/src/main/java/com/hydraxon91/backend/repositories/UserRepository/UserProfileRepository.java
+++ backend/src/main/java/com/hydraxon91/backend/repositories/UserRepository/UserProfileRepository.java
@@ -4,8 +4,11 @@ import com.hydraxon91.backend.models.UserModels.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+    Optional<UserProfile> findByUserName(String username);
+    Optional<UserProfile> findByUserId(UUID userId);
 }
