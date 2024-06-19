7b07b6b6186f37e628831ca12912ceaa87238640
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-19 10:37:40 +0200

    Update UserController.java

--- backend/src/main/java/com/hydraxon91/backend/controllers/authcontroller/UserController.java
+++ backend/src/main/java/com/hydraxon91/backend/controllers/authcontroller/UserController.java
@@ -2,9 +2,11 @@ package com.hydraxon91.backend.controllers.authcontroller;
 
 import com.hydraxon91.backend.models.UserModels.ApplicationUser;
 import com.hydraxon91.backend.repositories.UserRepository.UserRepository;
+import com.hydraxon91.backend.services.Authentication.ITokenServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,19 +16,23 @@ import java.util.UUID;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final ITokenServices tokenServices;
     
     @Autowired
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, ITokenServices tokenServices) {
         this.userRepository = userRepository;
+        this.tokenServices = tokenServices;
     }
     
     @GetMapping
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<ApplicationUser>> getAllUsers(){
         List<ApplicationUser> users = userRepository.findAll();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<ApplicationUser> getUserById(@PathVariable UUID id) {
         return userRepository.findById(id)
                 .map(ResponseEntity::ok)
@@ -40,6 +46,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id.toString() == principal.id")
     public ResponseEntity<ApplicationUser> updateUser(@PathVariable UUID id, @RequestBody ApplicationUser user) {
         if (!userRepository.existsById(id)) {return ResponseEntity.notFound().build();}
         user.setId(id);
@@ -48,6 +55,7 @@ public class UserController {
     }
     
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id.toString() == principal.id")
     public ResponseEntity<Void> deleteUserById(@PathVariable UUID id) {
         if (!userRepository.existsById(id)) {return ResponseEntity.notFound().build();}
         userRepository.deleteById(id);
