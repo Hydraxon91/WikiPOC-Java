a0fb7ede990fd34f4ad7da7354aedc7091c0c5ca
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-25 11:46:42 +0200

    Creates ArticlePageRepository.java

---
+++ backend/src/main/java/com/hydraxon91/backend/repositories/ArticleRepositories/ArticlePageRepository.java
@@ -0,0 +1,11 @@
+package com.hydraxon91.backend.repositories.ArticleRepositories;
+
+import com.hydraxon91.backend.models.ArticleModels.ArticlePage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ArticlePageRepository extends JpaRepository<ArticlePage, UUID> {
+}
