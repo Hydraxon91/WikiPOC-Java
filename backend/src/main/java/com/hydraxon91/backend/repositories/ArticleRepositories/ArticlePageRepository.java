0bc13ea4a834907170ae52b0279f74167da464b6
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-06-25 12:30:05 +0200

    Adds findBySlug to ArticlePageRepository

--- backend/src/main/java/com/hydraxon91/backend/repositories/ArticleRepositories/ArticlePageRepository.java
+++ backend/src/main/java/com/hydraxon91/backend/repositories/ArticleRepositories/ArticlePageRepository.java
@@ -4,9 +4,11 @@ import com.hydraxon91.backend.models.ArticleModels.ArticlePage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ArticlePageRepository extends JpaRepository<ArticlePage, UUID> {
     boolean existsBySlug(String slug);
+    Optional<ArticlePage> findBySlug(String slug);
 }
